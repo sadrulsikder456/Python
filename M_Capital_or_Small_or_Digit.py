ab493980888380e628ed01d35751d01883187871
sadrulsikder456 <129544832+sadrulsikder456@users.noreply.github.com>
2025-02-15 00:55:45 +0600

    Create M_Capital_or_Small_or_Digit.py
    1st code

---
+++ M_Capital_or_Small_or_Digit.py
@@ -0,0 +1,7 @@
+word=input()
+if (word>='0' and word<='9'):
+    print("IS DIGIT")
+elif (word>='a' and word<='z'):
+    print ("ALPHA\nIS SMALL")
+elif (word>='A' and word<='Z'):
+    print("ALPHA\nIS CAPITAL")
