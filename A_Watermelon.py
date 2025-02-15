d8708717db5b083e34a74b4351037f68c2f13343
sadrulsikder456 <129544832+sadrulsikder456@users.noreply.github.com>
2025-02-15 23:26:10 +0600

    69

---
+++ A_Watermelon.py
@@ -0,0 +1,7 @@
+wm=int(input()) # WM means weight of watermealon
+if wm>2 and wm%2==0:
+    print("YES")
+else:
+    print("NO")
+
+# logic of this problem is: if any integer is greater than 2 and modulus of 2 is 0 than it can be divide any even partion for 2 part
