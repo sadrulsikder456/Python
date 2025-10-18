b338b2910259b0e5989013f77c4e47dc78df5595
sadrulsikder456 <129544832+sadrulsikder456@users.noreply.github.com>
2025-10-18 23:40:40 +0600

    HackerRank

---
+++ Python_If_Else.py
@@ -0,0 +1,10 @@
+a=int(input())
+if a%2!=0:
+    print("Weird")
+else:
+    if 2<=a<=5:
+        print("Not Weird")
+    elif 6<=a<=20:
+        print("Weird")
+    else:
+        print("Not Weird")
