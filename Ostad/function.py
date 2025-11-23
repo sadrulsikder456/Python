c3a3f4de9f3ed4d33adfde6d18ae48c9ca1f34e3
sadrulsikder456 <129544832+sadrulsikder456@users.noreply.github.com>
2025-11-24 04:45:03 +0600

    Update function.py

--- Ostad/function.py
+++ Ostad/function.py
@@ -1,2 +1,12 @@
 user_name= input("Enter your name:")
 print(f"Hello {user_name}")
+
+def my_first_function():
+    print("This is my first function")
+
+my_first_function()
+
+def multiple(a,b):
+    return a*b
+
+print(multiple(5,6))
