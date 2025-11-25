1ff6bb55e3564eb6ef55e19681e64f5e4554ff9b
sadrulsikder456 <129544832+sadrulsikder456@users.noreply.github.com>
2025-11-26 02:39:33 +0600

    Add lambda function examples in lambda_function.py
    Introduces examples of lambda functions for squaring a number, adding two numbers, and sorting a list of students by score.

---
+++ Ostad/lambda_function.py
@@ -0,0 +1,7 @@
+square = lambda x: x*x
+print (square(500))
+add = lambda a,b : a+b
+print (add(200,300)) 
+students = [('rahim', 60),('karim', 70),('salim', 50)]
+sorted_student= sorted(students,key =lambda x: x[1])
+print (sorted_student)
