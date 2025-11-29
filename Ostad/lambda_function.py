92a917c468c0629071b077e4a52869b449fde9c9
sadrulsikder456 <129544832+sadrulsikder456@users.noreply.github.com>
2025-11-30 03:38:59 +0600

    Update lambda_function.py

--- Ostad/lambda_function.py
+++ Ostad/lambda_function.py
@@ -1,3 +1,4 @@
+from functools import reduce
 square = lambda x: x*x
 print (square(500))
 add = lambda a,b : a+b
@@ -5,3 +6,16 @@ print (add(200,300))
 students = [('rahim', 60),('karim', 70),('salim', 50)]
 sorted_student= sorted(students,key =lambda x: x[1])
 print (sorted_student)
+
+#map(), filter(), reduce()
+
+#map
+nums=[1,2,3,4]
+sq_nums= list(map(lambda x: x*x,nums))
+print(sq_nums)
+#filter
+even_nums= list(filter(lambda x: x%2==0,nums))
+print(even_nums)
+#reduce
+sum_nums= reduce (lambda a,b: a+b, nums)
+print(sum_nums)
