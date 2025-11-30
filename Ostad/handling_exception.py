38cf94a1ccd1b9e9da3be6a6664b6d1cf89a21d7
sadrulsikder456 <129544832+sadrulsikder456@users.noreply.github.com>
2025-12-01 02:31:32 +0600

    Improve exception handling and add file validation
    Commented out problematic int conversion, added else and finally blocks for better exception handling, and introduced a check_file function to validate .txt files in handling_exception.py. Simplified tempCodeRunnerFile.py to only include an int conversion statement.

--- Ostad/handling_exception.py
+++ Ostad/handling_exception.py
@@ -4,10 +4,21 @@ try:
         content = f.read()
         print(content)
     print(10/10)
-    x= int("abc")
+    # x= int("abc")
 except ZeroDivisionError:
     print("An error occurred: Division by zero is not allowed.")
 except FileNotFoundError:
         print("An error occurred while reading the file:", FileNotFoundError)
 except Exception as e:
     print("An unexpected error occurred:", e)
+else :
+    print("File read successfully and no exceptions occurred.")
+finally:
+    print("Execution completed.")
+def check_file(filename):
+    if not filename.endswith('.txt'):
+        raise ValueError("Only .txt files are allowed.")
+    print("File is valid.")
+check_file("Ostad/name.txt")
+    
+         
--- Ostad/tempCodeRunnerFile.py
+++ Ostad/tempCodeRunnerFile.py
@@ -1,4 +1,1 @@
-if os.path.exists('Ostad/name.txt'):
-#     print("File exists")
-# else :
-#     print("File does not exist")
+x= int("abc")
