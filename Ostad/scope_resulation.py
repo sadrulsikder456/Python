e305ae0335d5f2278e4582ccf07dcb28fd4e69c5
sadrulsikder456 <129544832+sadrulsikder456@users.noreply.github.com>
2025-11-30 17:50:44 +0600

    Add examples of variable scope and global keyword
    Expanded scope_resulation.py with additional examples demonstrating global, local, and enclosing variable scopes, as well as the use of the global keyword within nested functions.

--- Ostad/scope_resulation.py
+++ Ostad/scope_resulation.py
@@ -3,5 +3,20 @@ x=10 #Global variable
 print(x)
 def func():
     y=19 #Local variable
+    x=100
+    print(x)
     print(y)
 func()
+
+n="Global" #Global variable
+def outer():
+    
+    n="Enclosing" #Enclosing variable
+    def inner():
+        global n
+        n="Local" #Local variable
+        print(n)
+    inner()
+    print(n)
+outer()
+print(n)
