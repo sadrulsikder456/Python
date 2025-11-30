5a5a66260cfb0ba642a4d1422165f8967aa5c9f7
sadrulsikder456 <129544832+sadrulsikder456@users.noreply.github.com>
2025-11-30 18:08:01 +0600

    Append new lines to name.txt in file_handle.py
    Added code to append custom strings and a list of lines to 'Ostad/name.txt'. This enhances the script by allowing it to write additional content to the file.

--- Ostad/file_handle.py
+++ Ostad/file_handle.py
@@ -6,4 +6,11 @@
 with open("Ostad/name.txt", "r") as f:
     content = f.read()
     print(content)
-    
+
+with open("Ostad/name.txt", "a") as f:
+    f.write("\nI Love Rabeya")
+
+line= ['\nI Love Coding\n', 'I Love Python\n', 'I Love Bangladesh']
+
+with open("Ostad/name.txt", "a") as f:
+    f.writelines(line)
