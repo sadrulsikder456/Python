64fac90faf72bd1f699afeb1b7f4d43ec6546102
sadrulsikder456 <129544832+sadrulsikder456@users.noreply.github.com>
2025-12-01 01:55:05 +0600

    Add file existence and info checks to file_handle.py
    Introduced usage of os and pathlib to check for file existence, print absolute path, and display file size for 'Ostad/name.txt'. Also uncommented and restored initial file reading code.

--- Ostad/file_handle.py
+++ Ostad/file_handle.py
@@ -1,7 +1,9 @@
-# file = open("Ostad/name.txt", "r")
-# content = file.read()
-# print(content)
-# file.close()
+import os
+import pathlib
+file = open("Ostad/name.txt", "r")
+content = file.read()
+print(content)
+file.close()
 
 with open("Ostad/name.txt", "r") as f:
     content = f.read()
@@ -14,3 +16,14 @@ line= ['\nI Love Coding\n', 'I Love Python\n', 'I Love Bangladesh']
 
 with open("Ostad/name.txt", "a") as f:
     f.writelines(line)
+
+if os.path.exists('Ostad/name.txt'):
+    print("File exists")
+else :
+    print("File does not exist")
+
+file_path =pathlib.Path('Ostad/name.txt')
+if file_path.exists():
+    print("File exists")
+print(os.path.abspath('Ostad/name.txt'))
+print(os.path.getsize('Ostad/name.txt'))
