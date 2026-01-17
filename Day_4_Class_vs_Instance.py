7736551bc3093c18969b149483c3a33fc530c0f0
sadrulsikder456 <129544832+sadrulsikder456@users.noreply.github.com>
2026-01-18 01:34:37 +0600

    Update Day_4_Class_vs_Instance.py

--- Day_4_Class_vs_Instance.py
+++ Day_4_Class_vs_Instance.py
@@ -1,25 +1,28 @@
-class person:
-    def __init__ (self,age):
-        self._age=age
-    
-    def set_age(self,age):
-        if age<0:
-            print("Age cannot be valid, setting age to 0")
+class Person:
+    def __init__(self, initialAge):
+        if initialAge < 0:
+            print("Age is not valid, setting age to 0.")
+            self.age = 0
         else:
-            self._age=age
-    def get_age(self,age):
-        if age<0:
-            print("Age is not valid, setting age to 0")
-        elif (age>0 and age <13):
+            self.age = initialAge
+
+    def amIOld(self):
+        if self.age < 13:
             print("You are young.")
-        elif(age>=13 and age<18):
-            print("you are teenager.")
+        elif self.age >= 13 and self.age < 18:
+            print("You are a teenager.")
         else:
-            print("Your are old.")
+            print("You are old.")
+
+    def yearPasses(self):
+        self.age += 1
 
 t = int(input())
-
-for i in range(t):
-    age= int(input())
-    p=person(age)
-    p.get_age(age)
+for i in range(0, t):
+    age = int(input())         
+    p = Person(age)            
+    p.amIOld()                
+    for j in range(0, 3):      
+        p.yearPasses()       
+    p.amIOld()                
+    print("")                  
