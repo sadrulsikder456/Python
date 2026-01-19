bacb325e85f5fc116ce00ac1f04783e4232f66c4
sadrulsikder456 <129544832+sadrulsikder456@users.noreply.github.com>
2026-01-20 00:49:18 +0600

    Update file_handle.py

--- Ostad/Module3/file_handle.py
+++ Ostad/Module3/file_handle.py
@@ -1,3 +1,7 @@
-f = open("file.txt", "w")
+f = open("Ostad/Module3/file.txt", "a")
 f.write("I love you Rabeya")
 f.close()
+
+with open("Ostad/Module3/file.txt", "r") as f:
+    content = f.read()
+    print(content)
