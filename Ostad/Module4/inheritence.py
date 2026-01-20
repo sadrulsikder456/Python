127c9771fe515e3790e8b134892f46aa211e566d
sadrulsikder456 <129544832+sadrulsikder456@users.noreply.github.com>
2026-01-21 02:36:09 +0600

    Add Camera, GPS, and SmartPhone classes
    Introduced Camera and GPS classes with basic methods, and created a SmartPhone class demonstrating multiple inheritance. Added example usage of SmartPhone to call take_photo().

--- Ostad/Module4/inheritence.py
+++ Ostad/Module4/inheritence.py
@@ -10,3 +10,16 @@ class Student_account(BankAccount):
     pass
 s_acc = savings_account()
 s_acc.withdraw(5000)
+
+
+class Camera:
+    def take_photo(self):
+        print("Taking Photo")
+class GPS:
+    def get_location(self):
+        print("Getting Location")
+
+class SmartPhone(Camera,GPS):
+    pass
+sp=SmartPhone()
+sp.take_photo()
