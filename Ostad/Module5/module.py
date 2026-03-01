58a6e0e221ecf238e46a0a8f52d944d371311228
sadrulsikder456 <129544832+sadrulsikder456@users.noreply.github.com>
2026-03-02 01:51:20 +0600

    Update module.py

--- Ostad/Module5/module.py
+++ Ostad/Module5/module.py
@@ -1,15 +1,18 @@
 import math_op as mo
+import utils as u 
+import utils.hello as h
+from math_o import add
 
 def calculate_area_of_circle(radius):
     return mo.multiply(mo.multiply(radius, radius), 3.14159)
 print(calculate_area_of_circle(5))
 
-from math_o import add
+
 print(add(10, 20))
 
-import utils.hello as h
+
 h.rabeya()
 
-import utils as u
+ 
 u.hello.say()
 u.hello.rabeya()
