f8dfe8a4c388375b3a82bdee03f70f52ab9ee95d
sadrulsikder456 <129544832+sadrulsikder456@users.noreply.github.com>
2026-03-03 15:51:06 +0600

    Update datetime_demo.py

--- Ostad/Module5/datetime_demo.py
+++ Ostad/Module5/datetime_demo.py
@@ -2,3 +2,4 @@ import datetime as dt
 print(dt.datetime.now())
 print(dt.datetime.now().year)
 print(dt.datetime.now()-dt.datetime(2025,8,31))
+print(dt.datetime.strftime(dt.datetime.now(),"%B %d, %Y")) #%Y: year, %B: month, %d: day
