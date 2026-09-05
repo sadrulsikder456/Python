94574e138d4e4efea4a2955d0d97ff45110d466a
sadrulsikder456 <129544832+sadrulsikder456@users.noreply.github.com>
2026-09-05 22:17:14 +0600

    Create Reverse_a_number.py

---
+++ Gemini/Reverse_a_number.py
@@ -0,0 +1,9 @@
+number=int(input("Enter a number:"))
+reverse=0
+
+while number>0:
+    digit=number%10
+    reverse=reverse*10+digit
+    number//=10
+print("The reverse of the number is:",reverse)
+
