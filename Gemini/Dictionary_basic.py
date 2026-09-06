b2adc2ab60f1129282941d338822fbd8adfb75a5
sadrulsikder456 <129544832+sadrulsikder456@users.noreply.github.com>
2026-09-07 00:04:30 +0600

    Create Dictionary_basic.py

---
+++ Gemini/Dictionary_basic.py
@@ -0,0 +1,6 @@
+student={}
+student['name']=str(input("Enter your name:"))
+student['age']=int (input("Enter your age:"))
+student['department']=str(input("Enter your department:"))
+student['CGPA']=float(input("Enter your CGPA:"))
+print("Student details are:",student)
