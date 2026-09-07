95b4c5564da10d898631424bff3f786f17e400d7
sadrulsikder456 <129544832+sadrulsikder456@users.noreply.github.com>
2026-09-07 22:11:56 +0600

    Update palindrome_checker.py

--- Gemini/palindrome_checker.py
+++ Gemini/palindrome_checker.py
@@ -6,10 +6,12 @@ word=str(input("Enter a word:"))
 
 # this a also a tecnical way to check polindrome 
 
-# if is_palindrome(word):
-#     print(f"{word} is a palindrome")
-# else:
-#     print(f"{word} is not a palindrome")
+if is_palindrome(word):
+    print(f"{word} is a palindrome")
+else:
+    print(f"{word} is not a palindrome")
+
+#TODO: aita khane word ta function a diye boolian result niye oita if,else a diye check kore than print kora hocche.
 
 result=is_palindrome(word)
 if result==True:
