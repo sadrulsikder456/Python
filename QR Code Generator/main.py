24bf9b9ab75a1b36f33fab5e22a5e41c53e4f638
sadrulsikder456 <129544832+sadrulsikder456@users.noreply.github.com>
2026-09-17 00:53:23 +0600

    Create main.py

---
+++ QR Code Generator/main.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+
+root = tk.Tk()  # Tk() diye window poopup korano hoi
+
+root.geometry("500x500") # geometry() diye window ar size control kora hoi
+root.title("Main Window") # tittle() diye window ar head a tittle show korano hoi
+
+root.mainloop()
