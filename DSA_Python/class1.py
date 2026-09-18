c14c2ab5b55cd8b3167495c56340f3e1ea8fa4f8
sadrulsikder456 <129544832+sadrulsikder456@users.noreply.github.com>
2026-09-19 02:26:02 +0600

    Create class1.py

---
+++ DSA_Python/class1.py
@@ -0,0 +1,38 @@
+# List
+
+numbers = [1, 2, 3, 4, 4, 5]
+
+# print(numbers)
+
+# # noton kono element add kora
+# numbers.append(12)
+# print(numbers)
+
+# # kono specific index a element add kora
+# numbers.insert(2, 39)
+# print(numbers)
+
+# # element delete korar jonno
+# numbers.remove(4)
+# print(numbers)
+
+
+# # List traverse kora:
+
+# for num in numbers:
+#     print(num)
+
+# #Function
+
+# def greet(name):
+#     return f"Hello,{name}"
+
+# print(greet("Sadrul"))
+
+# def add(num1,num2):
+#     return num1+num2
+
+# print(add(2,4))
+
+
+print(numbers[:2])
